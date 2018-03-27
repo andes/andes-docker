213e0993aaf0827cfbdc6990aefca911bc361357
Mariano Botta <marianoabotta@gmail.com>
2018-03-27 11:51:08 -0300

    Backup

--- seed.sh
+++ seed.sh
@@ -1,6 +1,8 @@
 #!/bin/bash  
 
-docker exec  andes_elastic  curl -XPUT 'http://localhost:9200/andes/' -d '{"settings" : {"index" : { "number_of_shards" : 3, "number_of_replicas" : 2 } } }'
+#docker exec  andes_elastic  curl -XPUT 'http://localhost:9200/andes/' -d '{"settings" : {"index" : { "number_of_shards" : 3, "number_of_replicas" : 2 } } }'
+
+curl -XPUT 'http://localhost:9200/andes/' -d '{"settings" : {"index" : { "number_of_shards" : 3, "number_of_replicas" : 2 } } }'
 docker cp initial-db.gzip andes_db:/initial-db.gzip
 docker exec andes_db mongorestore --gzip --archive=/initial-db.gzip
 
