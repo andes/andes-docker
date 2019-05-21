c66e6a7b1e5c748c7b055f1cb35309e6565cfc5b
Mariano Andres Botta <marianoabotta@gmail.com>
2019-05-21 13:40:22 -0300

    add new index config

--- seed.sh
+++ seed.sh
@@ -2,7 +2,8 @@
 
 #docker exec  andes_elastic  curl -XPUT 'http://localhost:9200/andes/' -d '{"settings" : {"index" : { "number_of_shards" : 3, "number_of_replicas" : 2 } } }'
 
-curl -XPUT 'http://localhost:9200/andes/' -d '{"settings" : {"index" : { "number_of_shards" : 3, "number_of_replicas" : 2 } } }'
+curl -XPUT 'http://localhost:9200/andes/' -H "Content-Type: application/json" -d '{"settings" : {"index" : { "number_of_shards" : 3, "number_of_replicas" : 2 } } }'
+curl -XPUT -H 'Content-Type: application/json' 'http://localhost:9200/andes2/'   -d @andes2-index.json
 docker cp initial-db.gzip andes_db:/initial-db.gzip
 docker exec andes_db mongorestore --gzip --archive=/initial-db.gzip
 
